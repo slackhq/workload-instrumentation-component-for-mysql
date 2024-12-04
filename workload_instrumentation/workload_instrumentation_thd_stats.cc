1e65614d7f2d100e3b282507ca1c41553f6b11ea
Eduardo J. Ortega U. <5791035+ejortegau@users.noreply.github.com>
2024-12-04 15:54:33 +0100

    Fix assertion error (#1)
    This fixes an assertion error taking place when trying to access
    `affected rows` under some circumstances.
    
    Signed-off-by: Eduardo J. Ortega U. <5791035+ejortegau@users.noreply.github.com>

--- workload_instrumentation/workload_instrumentation_thd_stats.cc
+++ workload_instrumentation/workload_instrumentation_thd_stats.cc
@@ -1,11 +1,16 @@
 #include "workload_instrumentation_thd_stats.h"
 #include <sql/sql_class.h>
+#include <sql/sql_error.h>
 
 thread_stats* get_thd_row_stats(THD *thread) {
   auto ts = new thread_stats;
   ts->rows_examined = thread->get_examined_row_count();
   ts->rows_sent = thread->get_sent_row_count();
-  ts->rows_affected = thread->get_stmt_da()->affected_rows();
+  if (thread->get_stmt_da()->status() ==  Diagnostics_area::DA_OK) {
+    ts->rows_affected = thread->get_stmt_da()->affected_rows();
+  } else {
+    ts->rows_affected = 0;
+  }
 
   ts->start_time = &thread->start_time;
   ts->ustart_time = thread->start_utime;
